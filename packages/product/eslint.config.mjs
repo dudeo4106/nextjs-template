21f0a7bef9012640cfbca819a7d207228384c0ed
djung <57135015+dudeo4106@users.noreply.github.com>
2025-05-19 00:37:21 +0900

    refactor: add tsconfig to next directory (NON-ISSUE) (#8)
    * refactor: add tsconfig to next directory
    
    * refactor: adjust tsconfig and eslint

--- packages/product/eslint.config.mjs
+++ packages/product/eslint.config.mjs
@@ -1,4 +1,5 @@
 import { FlatCompat } from '@eslint/eslintrc';
+import rootEslintConfig from '../../eslint.config.mjs';
 
 const compat = new FlatCompat({
   // NOTE: import.meta.dirname is available after Node.js v20.11.0
@@ -7,11 +8,12 @@ const compat = new FlatCompat({
 
 // @see: https://nextjs.org/docs/app/api-reference/config/eslint#specifying-a-root-directory-within-a-monorepo
 const eslintConfig = [
+  ...rootEslintConfig, // extends root eslint config
   ...compat.config({
-    extends: ['next', 'next/typescript', 'prettier'],
+    extends: ['next', 'prettier'],
     settings: {
       next: {
-        rootDir: 'packages/product/',
+        rootDir: './',
       },
     },
   }),
