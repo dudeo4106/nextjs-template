e6d957c55b8a2905c11fc551bf3b091c8d986539
djung <dudeo4106@gmail.com>
2025-05-18 22:46:23 +0900

    refactor: add tsconfig to next directory

--- packages/product/next.config.ts
+++ packages/product/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   typescript: {
-    tsconfigPath: '../../tsconfig.app.json',
+    tsconfigPath: './tsconfig.json',
   },
 };
 
