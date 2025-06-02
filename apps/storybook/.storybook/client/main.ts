aae5ef759d59f772fef9764ff53f97e815000bd6
djung <57135015+dudeo4106@users.noreply.github.com>
2025-06-03 01:07:48 +0900

    chore: add alias for storybook (NON-ISSUE) (#32)

--- apps/storybook/.storybook/client/main.ts
+++ apps/storybook/.storybook/client/main.ts
@@ -1,8 +1,16 @@
+import path from 'node:path';
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config = {
   stories: ['../../../client/src/**/*.stories.@(ts|tsx)'],
   framework: '@storybook/nextjs',
+  webpackFinal: async (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      '@': path.resolve(__dirname, '../../../client/src'),
+    };
+    return config;
+  },
 } as const satisfies StorybookConfig;
 
 export default config;
