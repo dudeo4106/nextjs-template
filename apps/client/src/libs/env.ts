55544c8513234c5fb8ad70679a6abccef1d643ad
djung <dudeo4106@gmail.com>
2025-06-17 01:20:40 +0900

    feat: add env

---
+++ apps/client/src/libs/env.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production']),
+});
+
+const parsed = envSchema.safeParse(process.env);
+
+if (!parsed.success) {
+  console.error(
+    '❌ Invalid environment variables:',
+    parsed.error.flatten().fieldErrors,
+  );
+  throw new Error('Invalid environment variables');
+}
+
+export const env = parsed.data;
